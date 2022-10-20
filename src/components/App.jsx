8bbdcbab6f06c654aa9de367bd01f5ebcafd190d
mcpeck <peckmelodie@gmail.com>
2022-10-19 22:02:30 -0500

    Complete creating a dynamic Video List component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import exampleVideoData from '/compiled/src/data/exampleVideoData.js';
+import VideoList from '/compiled/src/components/VideoList.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +13,8 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+        <VideoList videos={exampleVideoData} />
       </div>
     </div>
   </div>
@@ -18,4 +22,5 @@ var App = () => (
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-export default App;
+
+export default App
